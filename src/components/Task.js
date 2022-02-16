c26bbba0aa37091a256bf7213a4d42e01176685a
HiddenOgre <yaslesd@gmail.com>
2022-02-15 22:14:43 -0300

    Fixed some layout problems

--- src/components/Task.js
+++ src/components/Task.js
@@ -13,7 +13,7 @@ const Task = ({todo, dispatch}) => {
   return (
     <div className='Task'>
         <input type="checkbox" className='Task__checkbox' onChange={handleToggleCompleted}></input>
-        <p className='Task__text'>{todo.text}</p>
+        <p className={todo.completed === false ?'Task__text Task__text--uncompleted' : 'Task__text Task__text--completed'}>{todo.text}</p>
         <button type='button' className='Task__btn' onClick={handleRemove}><img src={crossIcon} alt="" className='Task__btn__img'></img></button>
     </div>
   );

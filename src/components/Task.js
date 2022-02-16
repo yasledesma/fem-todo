949d69ca48e8fb02bc78c2698a8fcef440bbd503
HiddenOgre <yaslesd@gmail.com>
2022-02-16 10:47:27 -0300

    Finished filter logic

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import crossIcon from '../assets/icon-cross.svg';
 
-const Task = ({todo, dispatch}) => {
+const Task = ({task, dispatch}) => {
   const handleToggleCompleted = () => {
-    dispatch({type: "toggle-complete-task", payload: {id: todo.id}});
+    dispatch({type: "toggle-complete-task", payload: {id: task.id}});
   };
 
   const handleRemove = () => {
-    dispatch({type: "remove-task", payload: {id: todo.id}});
+    dispatch({type: "remove-task", payload: {id: task.id}});
   };
 
   return (
     <div className='Task'>
         <input type="checkbox" className='Task__checkbox' onChange={handleToggleCompleted}></input>
-        <p className={todo.completed === false ?'Task__text Task__text--uncompleted' : 'Task__text Task__text--completed'}>{todo.text}</p>
+        <p className={task.completed === false ? 'Task__text Task__text--uncompleted' : 'Task__text Task__text--completed'}>{task.text}</p>
         <button type='button' className='Task__btn' onClick={handleRemove}><img src={crossIcon} alt="" className='Task__btn__img'></img></button>
     </div>
   );
